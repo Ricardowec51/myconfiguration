e806d623cd92c8e38fab911b7a4135035816f251
Ricardowec51 <53792450+Ricardowec51@users.noreply.github.com>
2021-01-03 13:51:24 -0500

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
-echo "enter password"
-read pass
+
 sudo -s
-for question; do
-    read -p "${question}? "
-    replies=("${replies[@]}" "$REPLY")
-done
-echo "${replies[@]}"
+read -p "${question}? "
 apt upgrade -y
 apt install neofetch python3-pip -y
 pip3 install bpytop --upgrade
